d61ae7d2e67b11133b6073dc2a497a99dda4b51a
piyush-Remote <930piy@gmail.com>
2024-12-24 06:52:15 +0000

    INitial commit , but tailwind id not working

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-screen bg-red-600'>
+     <h1 className='text-red-950'>hello</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html', // Main HTML file, adjust path if needed
+    './src/**/*.{js,ts,jsx,tsx}', // Make sure this is correct for React (JSX/TSX files)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
